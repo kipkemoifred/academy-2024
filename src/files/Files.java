318c9b351a7e5d18aa67d6fa2a26db7131a22706
Kipkemoi Fred <fred.kipkemoi@tracom.co.ke>
2024-09-23 17:15:49 +0300

    feature: added files

---
+++ src/files/Files.java
@@ -0,0 +1,55 @@
+package files;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Files {
+    public static void main(String[] args) {
+        try {
+            File myObj = new File("filename.txt");
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+
+            FileWriter myWriter = new FileWriter("filename.txt");
+            myWriter.write("Files in Java might be tricky, but it is fun enough!");
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+
+
+
+
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+
+
+            if (myObj.exists()) {
+                System.out.println("File name: " + myObj.getName());
+                System.out.println("Absolute path: " + myObj.getAbsolutePath());
+                System.out.println("Writeable: " + myObj.canWrite());
+                System.out.println("Readable " + myObj.canRead());
+                System.out.println("File size in bytes " + myObj.length());
+            } else {
+                System.out.println("The file does not exist.");
+            }
+
+            if (myObj.delete()) {
+                System.out.println("Deleted the file: " + myObj.getName());
+            } else {
+                System.out.println("Failed to delete the file.");
+            }
+
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
